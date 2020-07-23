420488607de0111eab2c57b8387c903c1fb5fab2
Morrkof <ppipes@student.21-school.ru>
2020-07-23 22:14:32 +0300

    1

---
+++ ft_strlcpy.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlcpy.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ppipes <ppipes@student.21-school.ru>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/05/08 19:53:52 by ppipes            #+#    #+#             */
+/*   Updated: 2020/05/15 22:13:41 by ppipes           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+size_t	ft_strlcpy(char *dst, const char *src, size_t size)
+{
+	size_t	i;
+
+	i = 0;
+	if (src == NULL)
+		return (0);
+	while (i + 1 < size && src[i] != '\0')
+	{
+		dst[i] = src[i];
+		i++;
+	}
+	if (size > 0)
+		dst[i] = '\0';
+	return (ft_strlen(src));
+}
